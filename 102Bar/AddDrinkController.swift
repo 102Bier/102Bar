4aab4edaa7adbfbcfdec15d470f2e5664f2669a3
Justin Busse <jus@itmac06.local>
2018-03-22 20:42:45 +0100

    Complete functional list with drag and drop features

--- 102Bar/AddDrinkController.swift
+++ 102Bar/AddDrinkController.swift
@@ -17,31 +17,33 @@ class AddDrinkController : UIViewController, UITableViewDataSource, UITableViewD
         var sectionObjects : [String]!
     }
     
-    var objectsArray = [SectionAndObjects]()
-    
-    var availableIngredients = ["Wodka", "Gletscherwasser", "Wiskey", "Jägermeister"]
-    var selectedIngredients = ["Drag here to add stuff"]
+    var ingredArray = [SectionAndObjects]()
+    let helpText = "Drag here to add stuff"
+    let noMoreIngredientsText = "You greedy little bitch"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let availableIngredients = ["Wodka", "Gletscherwasser", "Wiskey", "Jägermeister", "Cola", "Fanta", "Organgensaft", "Red Bull"]
+        let selectedIngredients : [String] = [helpText]
+        
         tableView.setEditing(true, animated: true)
-        objectsArray = [SectionAndObjects(sectionName: "Selected ingredients", sectionObjects: selectedIngredients), SectionAndObjects(sectionName: "Available ingredients", sectionObjects: availableIngredients)]
+        ingredArray = [SectionAndObjects(sectionName: "Selected ingredients", sectionObjects: selectedIngredients), SectionAndObjects(sectionName: "Available ingredients", sectionObjects: availableIngredients)]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objectsArray[section].sectionObjects.count
+        return ingredArray[section].sectionObjects.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Custom drinks", for: indexPath)
         
-        cell.textLabel?.text = objectsArray[indexPath.section].sectionObjects[indexPath.row]
+        cell.textLabel?.text = ingredArray[indexPath.section].sectionObjects[indexPath.row]
         return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return objectsArray.count;
+        return ingredArray.count;
     }
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
@@ -50,30 +52,46 @@ class AddDrinkController : UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
-        let sourceSection = objectsArray[sourceIndexPath.section]
-        var destinationSection = objectsArray[destinationIndexPath.section]
+        let sourceIngredient = ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row]
+        //let destinationIngredient = ingredArray[destinationIndexPath.section].sectionObjects[destinationIndexPath.row]
         
-        let sourceIngredient = objectsArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row]
-        //let destinationIngredient = objectsArray[destinationIndexPath.section].sectionObjects[destinationIndexPath.row]
-        
-        if (objectsArray[sourceIndexPath.section].sectionName == "Available ingredients") && (!objectsArray[destinationIndexPath.section].sectionObjects.contains(sourceIngredient))
-            //if moving row from av ing to sel ing and sel ing doesn't already contain it
+        if !ingredArray[destinationIndexPath.section].sectionObjects.contains(sourceIngredient) &&
+            sourceIndexPath.section != destinationIndexPath.section &&
+            ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row] != helpText &&
+            ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row] != noMoreIngredientsText
+            //if destionation cell doesn't already contain it, and different sections are involved and no message text is selected --> adding
         {
-            objectsArray[destinationIndexPath.section].sectionObjects.insert(objectsArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row], at: destinationIndexPath.row)
+            ingredArray[destinationIndexPath.section].sectionObjects.insert(ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row], at: destinationIndexPath.row)
+            ingredArray[sourceIndexPath.section].sectionObjects.remove(at: sourceIndexPath.row)
         }
-
-        if((objectsArray[destinationIndexPath.section].sectionName == "Selected ingredients") &&     /*something was dragged to 1st section*/ (objectsArray[destinationIndexPath.section].sectionObjects.count >= 2) &&                /*array of dest Strings contains 2 Strings or more */
-            (objectsArray[destinationIndexPath.section].sectionObjects.contains("Drag here to add stuff"))) /*dest section has contains the help text*/
+        else if ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row] != helpText &&
+                ingredArray[sourceIndexPath.section].sectionObjects[sourceIndexPath.row] != noMoreIngredientsText
         {
-            objectsArray[destinationIndexPath.section].sectionObjects.remove(at:
-            objectsArray[destinationIndexPath.section].sectionObjects.index(of:
-            "Drag here to add stuff")!) //remove help text
+            ingredArray[sourceIndexPath.section].sectionObjects.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+        }
+        
+        if((ingredArray[destinationIndexPath.section].sectionName == "Selected ingredients") &&     /*something was dragged to 1st section*/ (ingredArray[destinationIndexPath.section].sectionObjects.count >= 2) /*array of dest Strings contains 2 Strings or more */ &&
+            (ingredArray[destinationIndexPath.section].sectionObjects.contains(helpText))) /*dest section has contains the help text*/
+        {
+            ingredArray[destinationIndexPath.section].sectionObjects.remove(at:
+            ingredArray[destinationIndexPath.section].sectionObjects.index(of:
+            helpText)!) //remove help text
+        }
+        if((ingredArray[sourceIndexPath.section].sectionName == "Selected ingredients") &&     /*something was dragged from 1st section*/ (ingredArray[sourceIndexPath.section].sectionObjects.isEmpty)) /*and 1st section is now empty*/
+        {
+            ingredArray[sourceIndexPath.section].sectionObjects.append(helpText) //show help text
+        }
+        
+        if((ingredArray[sourceIndexPath.section].sectionName == "Available ingredients") &&
+            ingredArray[sourceIndexPath.section].sectionObjects.isEmpty)
+        {
+            ingredArray[sourceIndexPath.section].sectionObjects.append(noMoreIngredientsText)
         }
         tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return objectsArray[section].sectionName
+        return ingredArray[section].sectionName
     }
     
     @IBAction func CancelTapped(_ sender: UIBarButtonItem) {
@@ -81,5 +99,6 @@ class AddDrinkController : UIViewController, UITableViewDataSource, UITableViewD
     }
     
     @IBAction func SaveTapped(_ sender: UIBarButtonItem) {
+        
     }
 }
