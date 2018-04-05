5b7d5ab9d13d8aca61830664f7377cab77925035
Christof Metzger <christof@metzgerle.com>
2018-04-05 14:40:32 +0200

    commented out something in appdrinkcontroller (flow --> int)

--- 102Bar/AddDrinkController.swift
+++ 102Bar/AddDrinkController.swift
@@ -439,11 +439,11 @@ class AddDrinkController : UITableViewController, UITextFieldDelegate
         var drinks : [Drink] = Array()
         for i in 0..<drinkContent.ingredArray[0].sectionObjects.count
         {
-            let drink = Drink(description: drinkContent.ingredArray[0].sectionObjects[i], percentage: Float(drinkContent.ingredArray[0].sectionPercentage[i])!)
-            drinks.append(drink)
+          //  let drink = Drink(description: drinkContent.ingredArray[0].sectionObjects[i], percentage: Float(drinkContent.ingredArray[0].sectionPercentage[i])!)
+          //  drinks.append(drink)
         }
         
         
-        customDrinkModel.addMix(mix: Mix(ingredients: drinks))
+        //customDrinkModel.addMix(mix: Mix(ingredients: drinks))
     }
 }
